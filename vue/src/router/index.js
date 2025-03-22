efa5c270245e0275a735bc346385d68f72196641
keithwzr <935084287@qq.com>
2025-03-22 23:16:38 +0800

    added card css and some components of manager.vue

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,11 +4,11 @@ import HomeView from '../views/manager/Home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/', redirect:'/manager/home',},
+    {path: '/', redirect:'/manager',},
     { path: '/404', component: () => import('@/views/404.vue')},
     { path: '/:pathMatch(.*)', redirect: '/404', hidden: true },
     {path:'/manager',component: () =>import('../views/Manager.vue'), children:[
-        {path:'home', meta: {name: '首页'}, component: () =>import('../views/manager/Home.vue')},
+        {path:'home', meta: {name: '系统首页'}, component: () =>import('../views/manager/Home.vue')},
       ]}
 
   ],
--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -16,14 +16,14 @@
 
       </div>
       <div class="manager-header-right">
-<!--        右侧头像和用户名      -->
+        <!-- 右侧头像和用户名 -->
         <el-dropdown style="cursor: pointer;">
-          <div style="padding-right: 20px; display: flex;align-items: center;">
-            <img v-if="data.user.avatar" src="" alt="">
-            <img v-else src="@/assets/img/avatar.png" alt="" style="width: 40px; height: 40px; ">
-            <span style="margin-left: 5px">{{data.user?.name}}</span>
+          <div style="padding-right: 20px; display: flex; align-items: center;">
+            <img v-if="data.user.avatar" :src="data.user?.avatar" alt="" style="width: 40px; height: 40px; display: block; border-radius: 50%">
+            <img v-else src="@/assets/imgs/avatar.png" alt="" style="width: 40px; height: 40px; display: block; border-radius: 50%">
+            <span style="margin-left: 5px; color: white">{{ data.user?.name }}</span>
           </div>
-          <template #deopdown>
+          <template #dropdown>
             <el-dropdown-menu>
               <el-dropdown-item @click.native="router.push('/manager/person')">个人资料</el-dropdown-item>
               <el-dropdown-item @click.native="router.push('/manager/password')">修改密码</el-dropdown-item>
@@ -31,7 +31,6 @@
             </el-dropdown-menu>
           </template>
         </el-dropdown>
-
       </div>
     </div>
 <!--    头部结束      -->
@@ -39,10 +38,27 @@
     <div style="display: flex">
       <div class="manager-main-left">
         <!-- 菜单区域 -->
+<!--        default-active 代表默认展开的路由-->
+        <el-menu :default-active="router.currentRoute.value.path"
+                 :default-openeds="['/manager/home', 'info']"
+                 router>
+          <el-menu-item index="/manager/home">
+            <el-icon><home-filled /></el-icon>
+            <span>系统首页</span>
+          </el-menu-item>
+          <el-sub-menu index="info">
+            <template #title>
+              <el-icon><UserFilled /></el-icon>
+              <span>信息管理</span>
+            </template>
+            <el-menu-item index="">xxx信息</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
       </div>
 
       <div class="manager-main-right">
         <!-- 内容区域 -->
+        <RouterView />
       </div>
     </div>
 <!--    主体区域结束        -->
@@ -56,6 +72,11 @@ export default {
 </script>
 <script setup>
 import router from "@/router/index.js"
+import {reactive} from "vue";
+const data = reactive({
+  user:{ name:'zhangsan' }
+})
+
 </script>
 <style scoped>
 @import "@/assets/css/manager.css";
